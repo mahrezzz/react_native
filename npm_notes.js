4968153c6fcf59b1368b35ac94e593a4384fad00
huseyinDaVinci <huseyin.davinci@gmail.com>
2016-12-02 17:15:37 +0300

    some notes added

---
+++ npm_notes.js
@@ -0,0 +1,4 @@
+npm install <package-name>   (--downlad a module)
+npm install --save <package-name>    (--add this dependency to package.json of the project)
+npm install --save-exact <package-name>@version    (--add this dependency to package.json of the project (strict to this version only))
+npm install --save-dev  <package-name>   (add this package under the devDependencies)
